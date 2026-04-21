0466e44f88404c367ee568b8e141d83d9e4b35a8
a18502656182-arch <a18502656182@gmail.com>
2026-04-21 16:21:31 +0800

    Update server.js

--- server.js
+++ server.js
@@ -91,7 +91,8 @@ mountApi("site_config");
 mountApi("star_log");
 mountApi("star_total");
 mountApi("reading_score_save");
-
+mountApi("seg_reaction_save");
+mountApi("seg_reaction_list");
 
 mountRsc("/rsc-api/clips", "./rsc-api/clips.js");
 mountRsc("/rsc-api/taxonomies", "./rsc-api/taxonomies.js");
